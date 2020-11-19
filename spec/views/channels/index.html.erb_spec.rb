cdfd660a2bebc103668fdeacc5b3972abc9b2bfa
Brian Davies <brian@briandavies.me>
2020-11-19 14:34:00 -0700

    Handle no channels in channel index view

--- spec/views/channels/index.html.erb_spec.rb
+++ spec/views/channels/index.html.erb_spec.rb
@@ -3,15 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe 'channels/index', type: :view do
-  it 'renders a list of channels' do
-    @channels = Kaminari.paginate_array(
-      [build_stubbed(:channel), build_stubbed(:channel)]
-    ).page(1)
+  context 'when there are channels to display' do
+    it 'renders a list of channels' do
+      @channels = Kaminari.paginate_array(
+        [build_stubbed(:channel), build_stubbed(:channel)]
+      ).page(1)
 
-    render
+      render
 
-    @channels.each do |channel|
-      expect(rendered).to match(/#{channel.title}/)
+      @channels.each do |channel|
+        expect(rendered).to match(/#{channel.title}/)
+      end
+    end
+  end
+
+  context 'when there are no channels' do
+    it "displays a 'no channels' message" do
+      @channels = Kaminari.paginate_array([]).page(1)
+
+      render
+
+      expect(rendered).to match(/No channels/)
     end
   end
 end
