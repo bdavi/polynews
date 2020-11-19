02f18a606b8d8ef634487eacfd7b476a903206db
Brian Davies <brian@briandavies.me>
2020-11-18 19:35:06 -0700

    Install Font Awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
+require("@fortawesome/fontawesome-free/js/all.js");
 
 // stylesheet
 require("../stylesheets/main.scss")
+require("@fortawesome/fontawesome-free/css/all.css")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
