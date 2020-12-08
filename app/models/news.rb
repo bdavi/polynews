b39df4b39bf17a87769c45549cf6e55c74f90b6d
Brian Davies <brian@briandavies.me>
2020-12-07 17:04:32 -0700

    Add Headlines to news

--- app/models/news.rb
+++ app/models/news.rb
@@ -5,22 +5,36 @@ class News
 
   def initialize(params)
     @params = params
+    @category = Category.find_by(slug: category_slug) || NullCategory.new(category_slug)
   end
 
   def category_slug
     params[:category]
   end
 
-  def category # rubocop:disable Lint/DuplicateMethods
-    @category ||= Category.find_by(slug: category_slug)
-  end
-
   def groups
-    Group
-      .where(category: category)
+    category
+      .groups
+      .joins(:category)
       .includes(articles: :channel)
       .order(cached_article_last_published_at: :desc)
       .page(params[:page])
       .per(20)
+  end
+
+  class NullCategory
+    attr_reader :slug
+
+    def initialize(slug)
+      @slug = slug
+    end
+
+    def title
+      slug.titleize
+    end
+
+    def groups
+      Group.where('cached_article_count > ?', 2)
+    end
   end
 end
